ea88990bc3d538feff558e7375fa7e447e3d7ba4
Farwhyn <jamesizhou@gmail.com>
2022-02-28 19:45:02 -0800

    bug fix: add brackets around second where clause

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { Brackets, EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
@@ -16,8 +16,12 @@ export class TasksRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
-        { search: `%${search}%` },
+        new Brackets((qb) => {
+          qb.where(
+            'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+            { search: `%${search}%` },
+          );
+        }),
       );
     }
 
